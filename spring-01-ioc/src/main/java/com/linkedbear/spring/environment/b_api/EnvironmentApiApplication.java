e4bdbbbacb379cc567ffa21a7321fb1ac80a45f5
LinkedBear <742532002@qq.com>
2020-11-10 16:31:56 +0800

    修复23章中包扫描丢失

--- spring-01-ioc/src/main/java/com/linkedbear/spring/environment/b_api/EnvironmentApiApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/environment/b_api/EnvironmentApiApplication.java
@@ -6,8 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class EnvironmentApiApplication {
     
     public static void main(String[] args) throws Exception {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
-                );
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext("com.linkedbear.spring.environment.b_api");
         EnvironmentHolder environmentHolder = ctx.getBean(EnvironmentHolder.class);
         environmentHolder.printEnvironment();
     }
