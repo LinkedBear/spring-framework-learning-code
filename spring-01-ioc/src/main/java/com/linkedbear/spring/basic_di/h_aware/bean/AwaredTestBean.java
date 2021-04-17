c1cfd67685392fe5a54d8b49808a4502c783e150
LinkedBear <742532002@qq.com>
2021-04-17 08:43:02 +0800

    修复basic_di.h_aware示例运行得不到预期结果的问题

--- spring-01-ioc/src/main/java/com/linkedbear/spring/basic_di/h_aware/bean/AwaredTestBean.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/basic_di/h_aware/bean/AwaredTestBean.java
@@ -4,9 +4,11 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 import java.util.stream.Stream;
 
+@Component
 public class AwaredTestBean implements ApplicationContextAware, BeanNameAware {
     
     private String beanName;
